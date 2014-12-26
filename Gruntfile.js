8143e26d8f64453d73e09d7dd5e6a848fe00cbc1
Madhur <ahuja.madhur@gmail.com>
2014-12-26 14:21:25 +0530

    more grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,13 +9,13 @@ module.exports = function(grunt)
         {
             options:
             {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+                banner: '/*! <%= pkg.description %> <%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
             },
             build:
             {
                 files:
                 {
-                    'build/emoji.min.js': ['js/app.js', 'js/config.js', 'js/emojiDirectives.js', 'js/emojiFilters.js']
+                    'build/emoji.min.js': ['js/app.js', 'js/config.js', 'js/emojiDirectives.js', 'js/emojiFilters.js', 'js/util.js', 'js/jquery.emojiarea.js', 'js/nanoscroller.js']
                 }
             }
         },
@@ -24,17 +24,35 @@ module.exports = function(grunt)
         {
             options:
             {
-                jshintrc: '.jshintrc'
+                jshintrc: '.jshintrc',
+                force: true,
+                reporterOutput: 'jshint.log'
             },
             all: ['Gruntfile.js', 'js/*.js']
+        },
+
+        cssmin:
+        {
+            target:
+            {
+                options:
+                {
+                    banner: '/*! <%= pkg.description %> <%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+                },
+                files:
+                {
+                    'build/emoji.min.css': ['css/style.css']
+                }
+            }
         }
     });
 
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
     // Default task(s).
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', ['uglify', 'jshint', 'cssmin']);
 
 };
